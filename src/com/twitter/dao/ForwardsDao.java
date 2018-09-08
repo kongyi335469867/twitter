dad6ce23de8dc9353ce6c5c25bae310fd6704692
dong13 <1774914923@qq.com>
2018-09-08 09:42:38 +0800

    更新文件 ForwardsDao.java

--- src/com/twitter/dao/ForwardsDao.java
+++ src/com/twitter/dao/ForwardsDao.java
@@ -3,8 +3,7 @@ package com.twitter.dao;
 import java.sql.Timestamp;
 import java.util.List;
 
-<<<<<<< HEAD
-import com.twitter.pojo.Forwards;
+import com.twitter.bean.Forwards;
 import com.twitter.util.DBUtil;
 
 public class ForwardsDao {
@@ -38,46 +37,6 @@ public class ForwardsDao {
 	}
 //转发
 	public int addForward(int tid, int stid, int uid, Timestamp ftime) {
-=======
-import com.twitter.bean.Forwards;
-import com.twitter.util.DBUtil;
-@SuppressWarnings("unchecked")
-public class ForwardsDao {
-	
-	//通过转发用户id和用户id查询关注信息，从第二条开始
-	public Forwards getForward(int tid, int uid) {
-		String sql = "select fid,tid, stid  from forwards where stid=? and uid=? limit 1";		
-		List<Forwards> list = DBUtil.query(Forwards.class, sql, tid, uid);
-		if (list.size() > 0)
-			return list.get(0);
-
-		return null;
-	}
-
-	//通过推文id和关注时间查询关注信息，从第二条开始
-	public Forwards getForward(int tid, Timestamp ftime) {
-		String sql = "select fid,tid, stid  from forwards where tid=? and ftime=? limit 1";
-		List<Forwards> list = DBUtil.query(Forwards.class, sql, tid, ftime);
-		if (list.size() > 0)
-			return list.get(0);
-
-		return null;
-	}
-
-	//判断是否有关注信息，如果有则输出true，没有则输出false
-	public boolean selForward(int uid, int stid) {
-		String sql = "select fid,tid, stid,uid  from forwards where uid=? and stid=? limit 1";
-		List<Forwards> list = DBUtil.query(Forwards.class, sql, uid, stid);
-
-		if (list.size() > 0) {
-			return true;
-		}
-
-		return false;
-	}
-	//添加数据
-	public int addForward(int tid, int stid, int uid, Timestamp ftime) {	
->>>>>>> branch 'master' of https://git.coding.net/JIMMY_MIN/twitter.git
 		String sql = "insert into forwards(tid, stid, uid,ftime) values(?,?,?,?)";
 		int n = DBUtil.update(sql, tid, stid, uid, ftime);
 		return n;
